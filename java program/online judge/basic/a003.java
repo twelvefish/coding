04fdc34c0c7568aa85ce6effdbd3abedbb2ebf1a
twelvefish <amingo08131@gmail.com>
2019-12-16 22:49:35 +0800

    a003

---
+++ java program/online judge/basic/a003.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+/**
+ * a003
+ */
+public class a003 {
+
+    public static void main(String[] args) {
+        Scanner scaner = new Scanner(System.in);
+        int month, day, sum;
+        while (scaner.hasNext()) {
+            month = scaner.nextInt();
+            day = scaner.nextInt();
+            sum = (month * 2 + day) % 3;
+            switch (sum) {
+            case 0:
+                System.out.println("普通");
+                break;
+            case 1:
+                System.out.println("吉");
+                break;
+            case 2:
+                System.out.println("大吉");
+                break;
+
+            default:
+                break;
+            }
+        }
+    }
+}
