ebb17b1fe45e8a97f046292333635602eb71cf1a
twelvefish <amingo08131@gmail.com>
2019-12-18 22:58:02 +0800

    a004

--- online judge/basic/a004/a004.java
+++ online judge/basic/a004/a004.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
+
 /**
  * a004
  */
 public class a004 {
 
     public static void main(String[] args) {
-        
+        int year = 0;
+        Scanner scaner = new Scanner(System.in);
+        while(scaner.hasNextInt()){
+            year = scaner.nextInt();
+            if(year % 400 == 0 || year % 4 == 0 && year % 100 != 0){
+                System.out.println("閏年");
+            }else{
+                System.out.println("平年");
+            }
+        }
     }
 }
